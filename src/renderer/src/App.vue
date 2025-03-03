435d9fbfd91bd08e61ce4ee59dcd778acb60fc07
Thiago Aio <thiagoa371@gmail.com>
2025-03-03 14:35:36 -0300

    feat: inicio

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Versions from './components/Versions.vue'
+
+const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+</script>
+
+<template>
+
+  <button @click="ipcHandle">Ping</button>
+
+  <p class="w-full bg-red-700 ">teste</p>
+
+  <Versions />
+</template>
